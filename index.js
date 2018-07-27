4a316be3e5ef1b3da731dfe4dff7cd57c4d6da22
Naomi Dennis <njdennis94@gmail.com>
2018-07-27 14:53:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function findMatching(list, value){
 function fuzzyMatch(list, value){
   return list.filter( (ele) => { return ele.slice(0, 2).toUpperCase()  === value.slice(0, 2).toUpperCase()  })
 }
+
+
+function matchName(list, value){
+  return list.filter( (ele) => {return ele['name'].toUpperCase() === value['name'].toUpperCase() })
+}
